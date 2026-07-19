88311120d07faea15bce7bed03e5ebcf0365a9d3
hardgs <javed1e3@gmail.com>
2026-07-19 03:27:00 -0700

    Fix Bug

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"stashcli/stashgram"
 )
 
-var cfg stashgram.Settings = stashgram.Settings{}
+var cfg = stashgram.Settings{}
 
 func main() {
+	// Get the directory of the executable
+	exePath, err := os.Executable()
+	if err != nil {
+		fmt.Println("Failed to get executable path:", err)
+		os.Exit(1)
+	}
+	configPath := filepath.Join(filepath.Dir(exePath), "settings.json")
 
-	/* Load Config */
-	if er := stashgram.LoadJSON("settings.json", &cfg); er != nil {
+	if er := stashgram.LoadJSON(configPath, &cfg); er != nil {
 		fmt.Println(er)
 		os.Exit(0)
 	}
